545ca22d4a970fcede49bb84b0e3836bf9d6508d
silambarasi <silambarasi@example.com>
2020-09-20 11:43:14 +0000

    Added a new print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
-// This is java print program
+// This is java print prograim
 public class HelloWorld {
-	   public static void main(String[] args) {
-		         
-		         System.out.println("Hello, World");
+	   public static void main(String[] args) { 
+		     System.out.println("Hello, World");
+		      System.out.println("Hello, Git");
+		      System.out.println("Hello, First Program");
+		 
 			    }
 		}		
